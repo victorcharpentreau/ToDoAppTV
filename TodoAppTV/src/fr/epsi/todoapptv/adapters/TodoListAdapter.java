4972c035a73d6982b16e75dbe37f5488b8438561
root <root@debian>
2014-05-21 00:27:56 +0200

    TodoListAdapter + load listView

--- TodoAppTV/src/fr/epsi/todoapptv/adapters/TodoListAdapter.java
+++ TodoAppTV/src/fr/epsi/todoapptv/adapters/TodoListAdapter.java
@@ -1,41 +1,48 @@
 package fr.epsi.todoapptv.adapters;
 
-import java.util.List;
+import java.util.ArrayList;
 
+import fr.epsi.todoapptv.R;
 import fr.epsi.todoapptv.pojos.Todo;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 public class TodoListAdapter extends BaseAdapter{
-	private List<Todo> data;
+	private ArrayList<Todo> data;
+	private Context context;
 	
-	public TodoListAdapter(List<Todo> todoList){
+	public TodoListAdapter(ArrayList<Todo> todoList, Context context){
 		this.data = todoList;
+		this.context = context;
 	}
 	
 	@Override
 	public int getCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return data.size();
 	}
 
 	@Override
-	public Object getItem(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public Object getItem(int pos) {
+		return data.get(pos);
 	}
 
 	@Override
-	public long getItemId(int arg0) {
-		// TODO Auto-generated method stub
-		return 0;
+	public long getItemId(int pos) {
+		return pos;
 	}
 
 	@Override
-	public View getView(int arg0, View arg1, ViewGroup arg2) {
-		// TODO Auto-generated method stub
-		return null;
+	public View getView(int position, View item, ViewGroup parent) {
+        LayoutInflater layoutInf = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        item = layoutInf.inflate(R.layout.todo_item, parent, false);
+        TextView titleView = (TextView) item.findViewById(R.id.todoTitle);
+        Todo todo = data.get(position);
+        titleView.setText(todo.getTodoStr());
+        return item;
 	}
 
 }
